04424fd6ad533746227c3b31faee964390c8e64f
John Car Lo <33212010+firemax13@users.noreply.github.com>
2020-05-23 08:15:55 +0800

    R1Q: Revert into r1q instead of sm-a805f

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,9 @@
- 
-#
-# Copyright (C) 2019 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# android_device_samsung_r1q
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sm_a805f.mk
+    $(LOCAL_DIR)/lineage_r1q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sm_a805f-userdebug \
-    lineage_sm_a805f-eng
+    lineage_r1q-userdebug \
+    lineage_r1q-user \
+    lineage_r1q-eng
