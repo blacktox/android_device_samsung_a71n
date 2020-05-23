dcc0a8fd29620548f0dcc87c694851c00848dfc5
John Car Lo <33212010+firemax13@users.noreply.github.com>
2020-05-23 08:18:58 +0800

    R1Q: Revert to R1Q instead

--- lineage_r1q.mk
+++ lineage_r1q.mk
@@ -1,18 +1,14 @@
-#
-# Copyright (C) 2019-2020 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-# aggiungere fle inherit
-# Inherit from sm_a805f
+# android_device_samsung_r1q
+
+# Inherit R1Q
 $(call inherit-product, device/samsung/sm_a805f/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_sm_a805f
-PRODUCT_DEVICE := sm_a805f
-PRODUCT_BRAND := Samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_GMS_CLIENTID_BASE := android-samsung 
+PRODUCT_NAME := lineage_r1q
+PRODUCT_DEVICE := r1q
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
